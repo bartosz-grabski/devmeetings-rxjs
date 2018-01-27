44dc4ab1083d914b358ff8d307692fee4b28c73e
katarzyna sender <kasiasender138@gmail.com>
2018-01-27 15:42:36 +0100

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule} from 'angularfire2/database';
-import { MatInputModule, MatButtonModule, MatListModule, MatDividerModule, MatFormFieldModule } from '@angular/material';
+import { MatInputModule, MatButtonModule, MatListModule, MatDividerModule, MatFormFieldModule, MatSidenavModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -11,6 +11,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageFormComponent } from './message-form/message-form.component';
+import { LinksComponent } from './links/links.component';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { MessageFormComponent } from './message-form/message-form.component';
     AppComponent,
     MessagesComponent,
     MessageFormComponent,
+    LinksComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,7 +32,8 @@ import { MessageFormComponent } from './message-form/message-form.component';
     MatFormFieldModule,
     ReactiveFormsModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
